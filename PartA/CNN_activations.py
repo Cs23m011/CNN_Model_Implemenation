951a70668ed635c1eff6d1cc2834f54d2f9d8ea6
Cs23m011 <162670195+Cs23m011@users.noreply.github.com>
2024-04-07 21:39:26 +0530

    Update CNN_activations.py

--- PartA/CNN_activations.py
+++ PartA/CNN_activations.py
@@ -1,11 +1,11 @@
 import torch.nn.functional as F
 class Activation_Function:
-    def activation_Function(self,activation_function):
-        if activation_function=='relu':
+    def activation_Function(self,activation_function):   # Check the provided activation function and return the corresponding function from torch.nn.functional
+        if activation_function=='relu':     # Rectified Linear Unit (ReLU) activation function
             return F.relu
-        if activation_function=='gelu':
+        if activation_function=='gelu':      # Gaussian Error Linear Unit (GELU) activation function
             return F.gelu
-        if activation_function=='selu':
+        if activation_function=='selu':       # Scaled Exponential Linear Unit (SELU) activation function
             return F.selu
-        if activation_function=='elu':
+        if activation_function=='elu':         # Exponential Linear Unit (ELU) activation function
             return F.elu
